2bd005b8b9a4d348f40ce1fcbdbbff9ddecd807d
yohana909 <yohananagy@icloud.com>
2024-11-24 03:02:04 -0800

    removed cleanDB.ts

--- server/src/seeds/seed.ts
+++ server/src/seeds/seed.ts
@@ -1,11 +1,11 @@
 import db from "../config/connection.js";
 import Question from "../models/Question.js";
-import cleanDB from "./cleanDb.js";
+// import cleanDB from "./cleanDb.js";
 
 import pythonQuestions from './pythonQuestions.json' assert { type: "json" };
 
 db.once('open', async () => {
-  await cleanDB('Question', 'questions');
+//  await cleanDB('Question', 'questions');
 
   await Question.insertMany(pythonQuestions);
 
